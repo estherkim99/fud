feb91822bd1eb95cb3fec3aae47cc1a00b073928
Esther Kim <estherkim1721@gmail.com>
2021-01-23 15:31:34 -0500

    initialize React project with router, firebase

---
+++ fud-front/src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SUNDER_ID,
+    appId: process.env.FIREBASE_API_KEY,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
